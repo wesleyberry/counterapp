2adeb07b4c7f8230e4476b26447c1d0bcfd2cfd7
Wesley Berry <wesleyberry52@gmail.com>
2019-12-18 12:11:01 -0600

    working event handler for increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,8 +6,10 @@ class Counter extends Component {
     };
 
     handleIncrement = () => {
-        console.log('Increment Clicked', this);
-    }
+        this.setState({
+            count: this.state.count + 1
+        });
+    };
 
     render() { 
         return  (
