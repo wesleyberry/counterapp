3e62360fd5180c09f062be8cf0babceeb2dd21e3
Wesley Berry <wesleyberry52@gmail.com>
2019-12-18 11:42:45 -0600

    dynamically updated classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
     };
 
     formatCount() {
@@ -10,11 +10,17 @@ class Counter extends Component {
         return count === 0 ? 'Zero' : count;
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-"
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
     render() { 
         return  (
             <React.Fragment>
-                <h1>{this.formatCount()}</h1>
-                <button>Increment</button>
+                <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
