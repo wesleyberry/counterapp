1f667423dac06a32e8ec856a06bea5ab6d2ec50d
Wesley Berry <wesleyberry52@gmail.com>
2019-12-18 11:28:13 -0600

    dynamically updated dom with counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0
+    };
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
     render() { 
-        return <h1>Hello World</h1>;
+        return  (
+            <React.Fragment>
+                <h1>{this.formatCount()}</h1>
+                <button>Increment</button>
+            </React.Fragment>
+        );
     }
 }
  
